be73d18e0c0a0978877a11d308fe218eea19262c
qqcat <jden4524@gmail.com>
2026-03-16 00:50:03 +1100

    add config for 2b model

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source attn_ft/bin/activate
-python src/attn_ft/train.py --config configs/qwen3_vl_8b_attn_ft_suppress.yaml
+python src/attn_ft/train.py --config configs/qwen3_vl_2b_attn_ft_suppress.yaml
