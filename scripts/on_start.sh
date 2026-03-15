270ab11e94b740a2fe84229945665dcae2e3a2bf
qqcat <jden4524@gmail.com>
2026-03-16 01:34:12 +1100

    add keep alive to bash

--- scripts/on_start.sh
+++ scripts/on_start.sh
@@ -3,4 +3,8 @@ git clone https://github.com/jden4524/attn_ft.git
 cd attn_ft
 bash scripts/setup_env.sh
 bash scripts/train.sh || echo "Training failed, proceeding to destroy..."
-vastai destroy instance $CONTAINER_ID
+if [ -f "$DATA_DIRECTORY/keep_alive" ]; then
+    echo "Lock file found. Staying online."
+else
+    vastai destroy instance $CONTAINER_ID
+fi
