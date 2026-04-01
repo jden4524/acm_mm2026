a1d9e576d126dd10811980384362bfc029c54a5c
qqcat <jden4524@gmail.com>
2026-04-02 02:51:15 +1100

    update eval setup

--- scripts/on_start_eval.sh
+++ scripts/on_start_eval.sh
@@ -3,6 +3,6 @@ git clone https://github.com/jden4524/attn_ft.git
 cd attn_ft
 cd eval
 git clone https://github.com/hengzhan/VLMEvalKit.git
-cd VLMEvalKit
-uv venv .venv
-uv pip install --python .venv/bin/python -e .
+uv venv eval
+source eval/bin/activate
+uv pip install -e ./VLMEvalKit
